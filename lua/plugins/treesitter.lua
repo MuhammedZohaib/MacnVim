f3ecbb787892bb7b5427f33fa426de0de26c3b11
Muhammad Zohaib <alizuhaib828@gmail.com>
2026-07-07 17:55:00 +0500

    feat: theme fix

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,51 @@
+local function has_core_treesitter()
+  if type(vim.treesitter) == "table" then
+    return true
+  end
+
+  vim.schedule(function()
+    vim.notify(
+      "Neovim core Treesitter API is unavailable. Restart Neovim after plugin/runtime updates; check `:version` if this repeats.",
+      vim.log.levels.ERROR
+    )
+  end)
+
+  return false
+end
+
+local parser_install_dir = vim.fn.stdpath("data") .. "/treesitter-parsers"
+
+local function ensure_parser_install_dir()
+  local parser_dir = parser_install_dir .. "/parser"
+  local ok = pcall(vim.fn.mkdir, parser_dir, "p")
+  if ok and vim.fn.isdirectory(parser_dir) == 1 then
+    vim.opt.runtimepath:prepend(parser_install_dir)
+    return parser_install_dir
+  end
+
+  vim.schedule(function()
+    vim.notify(
+      "Treesitter parser directory is not writable: " .. parser_install_dir,
+      vim.log.levels.WARN
+    )
+  end)
+
+  return nil
+end
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
     branch = "master",
     build = ":TSUpdate",
     event = { "BufReadPre", "BufNewFile" },
+    cond = has_core_treesitter,
     dependencies = {
       { "nvim-treesitter/nvim-treesitter-textobjects", branch = "master" },
-      "windwp/nvim-ts-autotag",
     },
+    init = function()
+      ensure_parser_install_dir()
+    end,
     config = function()
       local ok, ts_configs = pcall(require, "nvim-treesitter.configs")
       if not ok then
@@ -17,7 +55,7 @@ return {
         return
       end
 
-      ts_configs.setup({
+      local opts = {
         ensure_installed = {
           "bash",
           "css",
@@ -42,8 +80,13 @@ return {
         auto_install = false,
         highlight = {
           enable = true,
-          additional_vim_regex_highlighting = { "markdown" },
-          disable = { "markdown", "markdown_inline" },
+          disable = function(lang, bufnr)
+            local ft = vim.bo[bufnr].filetype
+            return lang == "markdown"
+              or lang == "markdown_inline"
+              or ft == "markdown"
+              or ft == "gitcommit"
+          end,
         },
         indent = {
           enable = true,
@@ -74,15 +117,26 @@ return {
             },
           },
         },
-      })
+      }
 
-      require("nvim-ts-autotag").setup({
-        opts = {
-          enable_close = true,
-          enable_rename = true,
-          enable_close_on_slash = true,
-        },
-      })
+      local parser_dir = ensure_parser_install_dir()
+      if parser_dir then
+        opts.parser_install_dir = parser_dir
+      end
+
+      ts_configs.setup(opts)
     end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    event = "InsertEnter",
+    cond = has_core_treesitter,
+    opts = {
+      opts = {
+        enable_close = true,
+        enable_rename = true,
+        enable_close_on_slash = true,
+      },
+    },
   },
 }
